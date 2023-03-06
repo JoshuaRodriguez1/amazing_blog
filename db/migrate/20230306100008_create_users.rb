a7955e5436547e54fb81696b6f76a3ec554857db
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-03-06 18:03:52 +0400

    constraints added in table users

--- db/migrate/20230306100008_create_users.rb
+++ db/migrate/20230306100008_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :firstname
+      t.string :username, null: false
+      t.string :firstname, null: false
       t.string :lastname
-      t.string :email
+      t.string :email, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_06_100008) do
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "firstname"
+    t.string "username", null: false
+    t.string "firstname", null: false
     t.string "lastname"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
