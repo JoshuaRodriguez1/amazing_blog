7d7c12b20649aa03e33782416c0fe81fa1aa2236
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-04-30 01:33:28 +0400

    find articles by tag

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,9 +2,12 @@ module Api
   module V1
     class ArticlesController < Api::V1::BaseController
       before_action :set_article, only: %i[show]
+      before_action :set_tag, if: ->{ tag_name_param.present? }
 
       def index
         @articles = Article.all
+
+        @articles = @tag.articles if @tag
         render json: @articles
       end
 
@@ -16,6 +19,14 @@ module Api
 
       def set_article
         @article = Article.find(params[:id])
+      end
+
+      def tag_name_param
+        params[:tagName]
+      end
+
+      def set_tag
+        @tag = Tag.find_by(name: tag_name_param)
       end
     end
   end
